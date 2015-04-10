cd8e610448b45459bea7e4f3f92616fa999e3b57
Kupluk <mirfanp@s.itb.ac.id>
2015-04-11 00:11:15 +0700

    Random
    Referensi kalau butuh hal yang random... kayak gua yang sering random
    juga.

---
+++ Randomizer.java
@@ -0,0 +1,15 @@
+import java.util.Random; //Harus ada ini
+
+public class Randomizer
+{
+	public static void main(String[] args)
+	{
+		Random rand = new Random(); //Konstruktor objek random
+	int i;
+	for(i=0;i<=10;i++){
+		int randomNum = rand.nextInt(41) + 1; //41 = max, 1 = min
+		System.out.println(randomNum);
+		}
+	}
+
+}
