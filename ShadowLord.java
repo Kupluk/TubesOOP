5885bb54e110c605753f9fc4258c382849d74250
IfanRamza <ifan.ramadhanzaki@gmail.com>
2015-04-11 22:04:49 +0700

    Create ShadowLord.java

---
+++ ShadowLord.java
@@ -0,0 +1,10 @@
+//KELAS MONSTER ShadowLord
+public class ShadowLord extends Monster {
+    
+    //KONSTRUKTOR
+    public ShadowLord () {
+        super("ShadowLord", 10, 60, 0, 50, 2);
+		System.out.println("ShadowLord telah muncul di arena");		
+    }
+    
+}
