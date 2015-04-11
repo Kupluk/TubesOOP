80b939c64be65e0421007430202ce374239e272c
IfanRamza <ifan.ramadhanzaki@gmail.com>
2015-04-11 22:02:23 +0700

    Create Creeps.java

---
+++ Creeps.java
@@ -0,0 +1,10 @@
+//KELAS MONSTER Creeps
+public class Creeps extends Monster {
+    
+    //KONSTRUKTOR
+    public Creeps () {
+        super("Creeps", 10, 60, 0, 50, 0);
+		System.out.println("Creeps telah muncul di arena");		
+    }
+    
+}
