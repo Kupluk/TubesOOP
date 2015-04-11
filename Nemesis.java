f48a82c369573a18c5f0fe21319d9c9128f031fc
IfanRamza <ifan.ramadhanzaki@gmail.com>
2015-04-11 22:03:55 +0700

    Create Nemesis.java

---
+++ Nemesis.java
@@ -0,0 +1,10 @@
+//KELAS MONSTER Nemesis
+public class Nemesis extends Monster {
+    
+    //KONSTRUKTOR
+    public Nemesis () {
+        super("Nemesis", 10, 60, 0, 50, 2);
+		System.out.println("Nemesis telah muncul di arena");		
+    }
+    
+}
