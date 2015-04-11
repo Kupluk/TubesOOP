b1dc2943d7266b4b340affaa2e1e888b3861991c
IfanRamza <ifan.ramadhanzaki@gmail.com>
2015-04-11 21:58:10 +0700

    Create Cerberus.java

---
+++ Cerberus.java
@@ -0,0 +1,10 @@
+//KELAS MONSTER Cerberus
+public class Cerberus extends Monster {
+    
+    //KONSTRUKTOR
+    public Cerberus () {
+        super("Cerberus", 10, 60, 0, 50, 1);
+		System.out.println("Cerberus telah muncul di arena");		
+    }
+    
+}
