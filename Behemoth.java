cb77b286a38812f67bf136728701913cee0ae06a
IfanRamza <ifan.ramadhanzaki@gmail.com>
2015-04-11 21:56:46 +0700

    Create Behemoth.java

---
+++ Behemoth.java
@@ -0,0 +1,11 @@
+//KELAS MONSTER BEHEMOTH
+public class Behemoth extends Monster {
+    
+    //KONSTRUKTOR
+    public Behemoth () {
+        super("Behemoth", 10, 60, 0, 50, 1);
+		System.out.println("Behemoth telah muncul di arena");		
+    }
+    
+    //SKILL
+}
