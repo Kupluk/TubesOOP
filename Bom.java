ffe0710fa606db87f4c27b49d4c616d75e132db8
muhirfan <muhirfan.mi@gmail.com>
2015-04-18 14:20:18 +0700

    Update Bom.java

--- Bom.java
+++ Bom.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Irfans
- */
 public class Bom extends Item {
     
     public Bom(){
-        super(10); //harga 10
+        super(10, 3); //harga 10
     }
 
-    @Override
     public void efekItem(Karakter k) {
     }
     public void efekItem(Monster m){
