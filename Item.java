4c3e72e057199c86a1632d710a9ed52c5c765cb3
muhirfan <muhirfan.mi@gmail.com>
2015-04-18 14:21:35 +0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -5,28 +5,38 @@
 //	-Minimal ada 3 item.
 //	-Buatlah item yang memiliki kegunaan yang unik.
 
-public abstract class Item {
-    private int harga;
+public class Item {
+    private int harga; 
+    private int idItem; 
     
     //konstruktor
     public Item(){
-        
+        harga=0; //harga urang default 0
+        idItem=0;//buat bedain tiap item, kalo item aja bukan subclass nya urang defaultin 0
     }
-    public Item(int h){
+    public Item(int h, int id){
         harga=h;
+        idItem=id;
     }
     
     //getter
     public int gerHarga(){
         return harga;
     }
+    public int getIdItem(){
+        return idItem;
+    }
     
     //setter
     public void setHarga(int h){
         harga=h;
     }
+    public void setIdItem(int id){
+        idItem=id;
+    }
     
     //method
-    public abstract void efekItem(Karakter k);
-    
+    //memberikan efek dari item ke karakter yang dipilih
+    public void efekItem(Karakter k){   
+    }
 }
